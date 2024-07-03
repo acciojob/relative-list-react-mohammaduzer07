a3b70c83f34da5515a99fafd3e0d76c33a6c0e80
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-07-03 15:27:42 +0530

    submit list

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleChange = () =>{
+    console.log(name);
+    setList([...list, name]);
+  }
+
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+
+      <input type='text' onChange={(e) => setName(e.target.value)}/>
+      <button onClick={handleChange}>Name List</button>
+      <ol key={'relativeList'}>
+        {
+          list.map((element, idx) =>{
+            return <li key={`relativeListitem${idx}`}>{element}</li>
+          })
+        }
+      </ol>
     </div>
   )
 }
